50dc86abc1540d1e600d9a707ea53e8c9ae63bd6
asifm.ashique <asifm.ashique@petronas.com>
2022-03-16 20:29:53 +0600

    update

--- src/app/layouts/SideBar.tsx
+++ src/app/layouts/SideBar.tsx
@@ -1,9 +1,26 @@
 import React, { useEffect, useState } from 'react';
+import {
+  faList,
+  faFilterCircleDollar,
+} from '@fortawesome/free-solid-svg-icons';
+import {} from '@fortawesome/free-regular-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { IoFunnelOutline, IoReorderFourOutline } from 'react-icons/io5';
 
 function SideBar() {
   return (
     <div className='sidebar'>
-      <h2>Side Bar</h2>
+      <div className='sidebar__box'>
+        <IoReorderFourOutline className='sidebar__icon' />
+
+        {/* <FontAwesomeIcon className='sidebar__icon' icon={faList} /> */}
+        <p className='sidebar__text'>Category</p>
+      </div>
+      <div className='sidebar__box'>
+        <IoFunnelOutline className='sidebar__icon' />
+
+        <p className='sidebar__text'>Filter</p>
+      </div>
     </div>
   );
 }
